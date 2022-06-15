4e75e45838d9d7692107a51adc34e0a5b91d8689
Vu Gia Khanh <khanh26102003a@gmail.com>
2022-06-15 18:07:19 +0700

    make it better

--- src/main/java/com/project/entity/MovieGenre.java
+++ src/main/java/com/project/entity/MovieGenre.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -11,13 +13,14 @@ import javax.persistence.ManyToOne;
 @Entity(name = "movie_genre")
 public class MovieGenre implements Serializable {
 	private static final long serialVersionUID = -7817224776021728682L;
-	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_movie")
 	private Movie movie;
 	
-	@Id
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_genre")
 	private Genre genre;
--- src/main/java/com/project/service/MovieService.java
+++ src/main/java/com/project/service/MovieService.java
@@ -2,7 +2,6 @@ package com.project.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
